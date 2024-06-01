37301b8e00d41af29a051daa43c10498372ace16
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2024-06-01 09:05:05 +0700

    Create data dummy in seeders
    make:migration 'add_phone_role_at_users' using table users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // Dinamis / otomatis membuatkan 10 user random
+        User::factory(10)->create(); // Data dummy 10 user
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Statis / Data Dummy Custom
+        User::factory()->create([
+            'name' => 'Admin Faisal',
+            'email' => 'faisal@gmail.com',
+            'password' => Hash::make('12345678'),
+        ]);
     }
 }
