e3710892d1fcccde1b2504c6f63d83529f56f9b4
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2024-06-01 11:22:27 +0700

    Add menu Users in sidebar and search user
    Read Only. Create, Update, Delete not yet

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    // index
+    public function index(Request $request) {
+        // Search by name, pagination
+        $users = DB::table('users')
+            ->when($request->input('name'), function ($query, $name) {
+                return $query->where('name', 'like', '%' . $name . '%');
+            })
+            ->orderBy('id', 'desc')
+            ->paginate(10);
+        return view('pages.users.index', compact('users'));
+    }
+}
